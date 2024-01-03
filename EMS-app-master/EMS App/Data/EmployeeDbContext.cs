1a17702fd100c81dcda0c74d115c7c1fc50a9e30
mayank5654 <mayankkaushal99@gmail.com>
2024-01-04 00:14:29 +0530

    intial commit

---
+++ EMS-app-master/EMS App/Data/EmployeeDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EMS_App.Models;
+
+namespace EMS_App.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext (DbContextOptions<EmployeeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EMS_App.Models.DeptMaster> DeptMaster { get; set; } = default!;
+
+        public DbSet<EMS_App.Models.EmpProfile>? EmpProfile { get; set; }
+    }
+}
